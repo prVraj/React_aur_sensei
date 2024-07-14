985587981e4f0f05b160da9405bbbea57e6be076
Vraj Parikh <vrajparikh169@gmail.com>
2024-07-14 14:22:02 +0530

    custome react

--- buddy/src/main.jsx
+++ buddy/src/main.jsx
@@ -1,10 +1,34 @@
 // import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx'
 
+// const reactElement = {
+//     type: "a",
+//     props: {
+//       href: "https://google.com",
+//       target: "_blank",
+//     },
+//     children: "Click me to visit google",
+//   };
+  
+// const anotherElement = (
+//     <a href="https://google.com" target="_blank">visit google</a>
+// );
+
+const reactElement = React.createElement(
+    'a',
+    {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    'Click me to visit google'
+
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
-    <App />
+    // anotherElement
+    reactElement
     
 )
--- customeReact/customeReact.js
+++ customeReact/customeReact.js
@@ -1,8 +1,20 @@
 function creatingElement(element, container) {
+  /*
+  // Version 1
+
   const domElement = document.createElement(element.type);
   domElement.innerText = element.children;
   domElement.setAttribute("href", element.props.href);
   domElement.setAttribute("target", element.props.target);
+  container.appendChild(domElement);
+  */
+  // Version 2
+
+  const domElement = document.createElement(element.type);
+  domElement.innerText = element.children;
+  for (let prop in element.props) {
+    domElement.setAttribute(prop, element.props[prop]);
+  }
   container.appendChild(domElement);
 }
 
