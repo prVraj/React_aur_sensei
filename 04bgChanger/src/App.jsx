f1dec5935e04c84ebe9406b5f90f42748860d22a
Vraj Parikh <vrajparikh169@gmail.com>
2024-07-16 16:31:48 +0530

    04bgChanger Done!!

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,13 +1,52 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [color, setColor] = useState(0)
+  const [color, setColor] = useState("gray");
 
   return (
-    <>
-    
-    </>
-  )
+    <div className="w-full h-screen " style={{ backgroundColor: color }}>
+      <div
+        className="fixed flex flex-wrap justify-center gap-3 inset-x-4 bottom-14 rounded-2xl"
+        // style={{ backgroundColor: "white" }}
+      >
+        <div
+          className="flex flex-wrap p-2 outline-1 rounded-2xl justify-centergap-2"
+          style={{ backgroundColor: "red" }}
+        >
+          <button
+            className="flex flex-wrap justify-center rounded-md"
+            onClick={() => setColor("red")}
+          >
+            red
+          </button>
+          </div>
+          <div
+            className="flex flex-wrap p-2 outline-1 rounded-2xl justify-centergap-2"
+            style={{ backgroundColor: "blue" }}
+          >
+            <button
+              className="flex flex-wrap justify-center rounded-md"
+              onClick={() => setColor("blue")}
+            >
+              blue
+            </button>
+            </div>
+            <div
+              className="flex flex-wrap p-2 outline-1 rounded-2xl justify-centergap-2"
+              style={{ backgroundColor: "green" }}
+            >
+              <button
+                className="flex flex-wrap justify-center rounded-md"
+                onClick={() => setColor("green")}
+              >
+                green
+              </button>
+            
+          
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
